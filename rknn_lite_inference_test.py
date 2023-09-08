19ee4a04a61f50379fe8df0dd8c7af624cad6bf7
Besserwisser1 <einbesserwisser@yandex.ru>
2023-09-08 18:23:19 +0700

    add test inference

--- rknn_lite_inference_test.py
+++ rknn_lite_inference_test.py
@@ -1,6 +1,7 @@
 import os, cv2, time, numpy as np
 from utils import *
 from rknnlite.api import RKNNLite
+import traceback
 
 conf_thres = 0.25
 iou_thres = 0.45
@@ -22,12 +23,21 @@ if __name__ == '__main__':
     isExist = os.path.exists(result_path)
     if not isExist:
         os.makedirs(result_path)
+
     rknn_lite = RKNNLite(verbose=False)
     ret = rknn_lite.load_rknn(RKNN_MODEL)
+    if ret != 0:
+        print('Load model failed!')
+        exit(ret)
+
     ret = rknn_lite.init_runtime()
-    if video_inference == True:
+    if ret != 0:
+        print('Init runtime environment failed!')
+        exit(ret)
+
+    try:
         cap = cv2.VideoCapture(video_path)
-        while(True):
+        while(cap.isOpened()):
             ret, image_3c = cap.read()
             if not ret:
                 break
@@ -49,25 +59,13 @@ if __name__ == '__main__':
                 print('--> Save inference result')
             else:
                 print("No Detection result")
-            cv2.waitKey(10)
-    else:
-        image_3c = cv2.imread(image_path)
-        image_4c, image_3c = preprocess(image_3c, input_height, input_width)
-        ret = rknn_lite.init_runtime()
-        start = time.time()
-        outputs = rknn_lite.inference(inputs=[image_3c])
-        stop = time.time()
-        fps = round(1/(stop-start), 2)
-        outputs[0]=np.squeeze(outputs[0])
-        outputs[0] = np.expand_dims(outputs[0], axis=0)
-        colorlist = gen_color(len(CLASSES))
-        results = postprocess(outputs, image_4c, image_3c, conf_thres, iou_thres, classes=len(CLASSES)) ##[box,mask,shape]
-        results = results[0]              ## batch=1
-        boxes, shape = results
-        if isinstance(boxes, np.ndarray):
-            vis_img = vis_result(image_3c,  results, colorlist, CLASSES, result_path)
-            print('--> Save inference result')
-        else:
-            print("No Detection result")
-    print("RKNN inference finish")
-    rknn_lite.release()
+            cv2.waitKey(5)
+    except Exception:
+        pass
+    finally:
+        # print("[INFO] elapsed time: {:.2f}".format(fps.elapsed()))
+        # print("[INFO] approx. FPS: {:.2f}".format(fps.fps()))
+        # release the cap and rknn thread pools
+        cap.release()
+        cv2.destroyAllWindows()
+        rknn_lite.release()
